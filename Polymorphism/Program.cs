97b4fbdc247e0a3aba0a2825dcc9d9ea3c90631d
Rob Horton <robhorton@outlook.com>
2017-11-07 15:39:51 -0600

    Updating order amounts and adding some comments to make the codes intent more clear.

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -10,25 +10,27 @@ namespace Polymorphism
     {
         static void Main(string[] args)
         {
-            var orderlist = OrderProcessingQueueSetup();
+            var orderlist = OrderProcessingQueueSetup();  // Get our mocked up queue of orders.
 
+            // Simulate processing each order in the list, get its type and calculate the discount
+            // based on the type of order.
             foreach (var order in orderlist)
             {
                 Console.WriteLine($"Order type is: {order.Type}. Order subtotal: ${order.Subtotal}, the discount is: ${order.GetDiscount}, and the final total is: ${order.GetOrderTotal}");
             }
-
             Console.ReadLine();
-
         }
 
+        // This method mocks up a queue of orders that should be processed.  The discount will 
+        // need to be applied to each order as their are processed in Main.
         private static List<OrderBase> OrderProcessingQueueSetup()
         {
             List<OrderBase> orderlist = new List<OrderBase>();
 
-            OrderBase order1 = new OrderBase { Subtotal = 100 };
-            OrderCustomerAbc order2 = new OrderCustomerAbc { Subtotal = 100 };
-            OrderCustomerDef order3 = new OrderCustomerDef { Subtotal = 100 };
-            OrderCustomerXyz order4 = new OrderCustomerXyz { Subtotal = 100 };
+            OrderBase order1 = new OrderBase { Subtotal = 275 };
+            OrderCustomerAbc order2 = new OrderCustomerAbc { Subtotal = 380 };
+            OrderCustomerDef order3 = new OrderCustomerDef { Subtotal = 1233 };
+            OrderCustomerXyz order4 = new OrderCustomerXyz { Subtotal = 899 };
 
             orderlist.Add(order1);
             orderlist.Add(order2);
